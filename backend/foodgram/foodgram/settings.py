61dfbb61baefcf00c1efd799e9dbfa91cccb6651
Evgenii <norugin@gmail.com>
2025-03-15 05:43:09 +0300

    5 try ci/cd

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -131,9 +131,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = '/static_backend/'
 STATIC_ROOT = '/backend_static/'
 
