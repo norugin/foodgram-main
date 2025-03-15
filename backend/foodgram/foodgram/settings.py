b5973522ea2899eff75ad9f3f7b0443a5f511038
Evgenii <norugin@gmail.com>
2025-03-15 05:37:27 +0300

    4 try ci/cd

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -139,8 +139,7 @@ STATIC_ROOT = '/backend_static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/app/media/'
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
